335f75b2d67e942d3be058be5300b41711676053
Gilliady <gilliadyazevedo@gmail.com>
2023-01-22 18:08:33 -0300

    realizado primeiro exercício de Fixação de Variáveis.

---
+++ fundamentos/secao-03-Introduao-a-JavaScript/exercicios/Scripts-exercicio-fixacao-01.js
@@ -0,0 +1,6 @@
+const myName = 'Gilliady';
+const birthCity = 'Pinhais';
+let birthYear = 1999;
+//birthYear = 2030;
+//birthCity = 'Curitiba';
+console.log('Meu nome é ' + myName + ', nasci em ' +  birthCity + ' no ano de ' + birthYear);
