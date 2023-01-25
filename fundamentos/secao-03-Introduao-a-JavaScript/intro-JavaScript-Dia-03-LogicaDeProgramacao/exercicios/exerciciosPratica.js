090e608682b6b9724c0421b2eb826d7f3cc0e28d
Gilliady <gilliadyazevedo@gmail.com>
2023-01-25 12:59:12 -0300

    finalizado exercício de menor palavra e maior palavra.

--- fundamentos/secao-03-Introduao-a-JavaScript/intro-JavaScript-Dia-03-LogicaDeProgramacao/exercicios/exerciciosPratica.js
+++ fundamentos/secao-03-Introduao-a-JavaScript/intro-JavaScript-Dia-03-LogicaDeProgramacao/exercicios/exerciciosPratica.js
@@ -5,10 +5,22 @@ for (let index = fatorial - 1; index <= fatorial && index > 0; index -= 1) {
     console.log(result);
 }
 
-
 let word = 'tryber';
 let invertedWord = '';
 for (let index = word.length -1; index >= 0; index -= 1) {
   invertedWord += word[index];
   console.log(invertedWord);
 }
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let smallerWord = array[0];
+let higherWord = '';
+for (let index = 0; index < array.length; index += 1) {
+  if(smallerWord.length > array[index].length){
+    smallerWord = array[index];
+  }else if(higherWord.length < array[index].length){
+    higherWord = array[index];
+  } 
+}
+console.log(smallerWord);
+console.log(higherWord);
