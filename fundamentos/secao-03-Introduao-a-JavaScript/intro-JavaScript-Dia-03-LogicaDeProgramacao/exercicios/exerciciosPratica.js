47ab5848b69bc9695ddf32aa01da173790d9dcf0
Gilliady <gilliadyazevedo@gmail.com>
2023-01-25 12:53:45 -0300

    finalizado inverter frase

--- fundamentos/secao-03-Introduao-a-JavaScript/intro-JavaScript-Dia-03-LogicaDeProgramacao/exercicios/exerciciosPratica.js
+++ fundamentos/secao-03-Introduao-a-JavaScript/intro-JavaScript-Dia-03-LogicaDeProgramacao/exercicios/exerciciosPratica.js
@@ -4,3 +4,11 @@ for (let index = fatorial - 1; index <= fatorial && index > 0; index -= 1) {
     result *= index;
     console.log(result);
 }
+
+
+let word = 'tryber';
+let invertedWord = '';
+for (let index = word.length -1; index >= 0; index -= 1) {
+  invertedWord += word[index];
+  console.log(invertedWord);
+}
