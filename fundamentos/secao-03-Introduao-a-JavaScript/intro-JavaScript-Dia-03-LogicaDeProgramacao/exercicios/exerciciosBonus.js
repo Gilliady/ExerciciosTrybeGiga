998cf5337b1926e805d68c8116929b43c83eeae0
Gilliady <gilliadyazevedo@gmail.com>
2023-01-26 12:12:26 -0300

    comitado

--- fundamentos/secao-03-Introduao-a-JavaScript/intro-JavaScript-Dia-03-LogicaDeProgramacao/exercicios/exerciciosBonus.js
+++ fundamentos/secao-03-Introduao-a-JavaScript/intro-JavaScript-Dia-03-LogicaDeProgramacao/exercicios/exerciciosBonus.js
@@ -1,40 +1,49 @@
-let n = 5;
-let word = '';
-for (let index = 0; index < n; index += 1) {
-  word = '';
-  for(let index2 = 0; index2 < n; index2 += 1){
-    word += '*';
-        
-    }
-    console.log(word);
-}
-word = '';
-for (let index = 0; index < n; index += 1) {
-  word += '*';
-  console.log(word);     
-}
-word = '';
-for (let index = 0; index < n; index += 1) {
-  word = '';
-  for (let index2 = 0; index2 < n; index2++) {
-    word += ' ';
-  }
-  word[0].replace = '*';
-  console.log(word);
-   
-}
+/* let jogadas = ["pedra", "papel", "tesoura"];
+let playComputador = jogadas[Math.floor(Math.random() * jogadas.length)];
+let playPlayer = jogadas[2];
 
-let number = 3;
-let isPrime = true;
-for(let index = 0; index < number; index += 1){
-  if(number % index === 0 && index != 1 && index != number){
-    isPrime = false;
-  }
+console.log(playPlayer + ' ' + playComputador);
 
+if(playPlayer === playComputador){
   
-}
-if(isPrime == true){
-  console.log(number + ' é um número primo');
+  console.log('A Ties');
+
+}else {
+  switch(playPlayer){
+    case jogadas[0]:
+    if(playComputador == jogadas[1]){
+      console.log('Computador vence.');
+    }else {
+      console.log('Jogador vence.');
+    }
+    break;
+    case jogadas[1]:
+      if(playComputador == jogadas[2]){
+        console.log('Computador vence.');
+      }else{
+        console.log('Jogador vence.');
+      }
+    break;
+    case jogadas[2]:
+      if(playComputador == jogadas[0]){
+        console.log('Computador vence.');
+      }else{
+        console.log('Jogador vence.');
+      }
+    break;
+  }
+} */
+
+let carolIdade = 25;
+let flavioIdade = 33;
+let noelIdade = 45;
+let idades =  [carolIdade, flavioIdade, noelIdade];
+let menorIdade = null;
+
+if(carolIdade < flavioIdade && carolIdade < noelIdade){
+  console.log('Carolzita é a pessoa mais nova');
+}else if( flavioIdade < carolIdade && flavioIdade < noelIdade){
+  console.log('Flavio é a pessoa mais nova.');
 }else{
-  console.log(number + ' não é um número primo');
+  console.log('Noel é a pessoa mais nova.');
 }
