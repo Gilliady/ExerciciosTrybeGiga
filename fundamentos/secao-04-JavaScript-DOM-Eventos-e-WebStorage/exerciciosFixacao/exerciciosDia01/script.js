1ea6df03cb65d079be88d398707579e65b1f290f
Gilliady <gilliadyazevedo@gmail.com>
2023-01-31 16:17:08 -0300

    finalizado exercicios 2

--- fundamentos/secao-04-JavaScript-DOM-Eventos-e-WebStorage/exerciciosFixacao/exerciciosDia01/script.js
+++ fundamentos/secao-04-JavaScript-DOM-Eventos-e-WebStorage/exerciciosFixacao/exerciciosDia01/script.js
@@ -2,14 +2,21 @@ const title = document.getElementById("page-title");
 
 const subTitle = document.getElementById("subtitle");
 
+const subTitleByTag = document.getElementsByTagName('h4');
+
 const paragraph = document.getElementById("paragraph");
 
 const secondParagraph = document.getElementById("second-paragraph");
+
+const paragraphs = document.getElementsByClassName('paragraphs');
 
 title.innerText = 'Baby Driver';
 
 subTitle.innerText = 'Dirigindo e curtindo sonzera';
 
-paragraph.style.color = "blue";
+subTitleByTag[0].style.color = 'pink';
+for (let paragraph = 0; paragraph < paragraphs.length; paragraph++) {
+  paragraphs[paragraph].style.color = 'green';  
+}
 
 secondParagraph.innerText = 'DK-san'
