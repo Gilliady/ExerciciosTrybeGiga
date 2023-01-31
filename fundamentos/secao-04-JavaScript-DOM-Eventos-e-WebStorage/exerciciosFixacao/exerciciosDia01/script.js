229bd4ef85901de4e78e3e4ef23f7e27306e4c64
Gilliady <gilliadyazevedo@gmail.com>
2023-01-31 15:49:20 -0300

    fiz o primeiro exercicio de fixação e esqueci de passar pra branch do dia

---
+++ fundamentos/secao-04-JavaScript-DOM-Eventos-e-WebStorage/exerciciosFixacao/exerciciosDia01/script.js
@@ -0,0 +1,15 @@
+const title = document.getElementById("page-title");
+
+const subTitle = document.getElementById("subtitle");
+
+const paragraph = document.getElementById("paragraph");
+
+const secondParagraph = document.getElementById("second-paragraph");
+
+title.innerText = 'Baby Driver';
+
+subTitle.innerText = 'Dirigindo e curtindo sonzera';
+
+paragraph.style.color = "blue";
+
+secondParagraph.innerText = 'DK-san'
